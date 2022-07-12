eb9a526d4dd6ef183baa365798960817ccb36f49
drewwbikk <70914096+drewwbikk@users.noreply.github.com>
2022-07-11 21:36:46 -0500

    Week 6 work on script

--- new_iso.py
+++ new_iso.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import subprocess, sys
 
+# Parse arguments
 parser = argparse.ArgumentParser(description="Enumerate an ISO file.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("iso", help="Full ISO File Location")
 parser.add_argument("os", help="Name of the OS")
@@ -15,8 +16,11 @@ osType = config['type']
 winIsoPath = ""
 
 def mount_iso():
-    p_cmd = r"C:\Users\Drew\Desktop\ISOs\new_iso.ps1 -i " + iso + " -os " + osName + " -type " + osType
+    # Set the command to run the PowerShell script to mount, enumerate, and parse all file paths of a new ISO/OS
+    p_cmd = r"C:\Users\Drew\Desktop\ISOs\week6\new_iso.ps1 -i " + iso + " -os " + osName + " -type " + osType
+    # Run the command in Powershell, and direct output to stdout
     p = subprocess.Popen(['powershell.exe', p_cmd], stdout=sys.stdout)
+    # Redirect output to console
     p.communicate()
 
 mount_iso()
